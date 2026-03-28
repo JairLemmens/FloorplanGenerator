8260f9aee8cba2fc3bff1db0f4a98c92b29f7d9d
Jair Lemmens <Jairlemmens@gmail.com>
2026-03-28 10:25:37 +0100

    Partially transformation lock alignment fix

--- Grasshopper Source/JLP/obj/Release/net7.0-windows/JLP.AssemblyInfo.cs
+++ Grasshopper Source/JLP/obj/Release/net7.0-windows/JLP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of JLP")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5a86a85e170ac8ced8d8dd74b9329734e98808e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+22408defc75dcbc6ab45794130e77c2b92636e07")]
 [assembly: System.Reflection.AssemblyProductAttribute("JLP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JLP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Grasshopper Source/PythonComponents/rh8/src/JLP.Components/AssemblyInfo.cs
+++ Grasshopper Source/PythonComponents/rh8/src/JLP.Components/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using Rhino.PlugIns;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("2ceebce6-c31e-4289-abb1-b9cd21b65791")]
-[assembly: AssemblyVersion("0.1.9583.34442")]
-[assembly: AssemblyFileVersion("0.1.9583.34442")]
-[assembly: AssemblyInformationalVersion("0.1.9583.34442")]
+[assembly: AssemblyVersion("0.1.9583.16545")]
+[assembly: AssemblyFileVersion("0.1.9583.16545")]
+[assembly: AssemblyInformationalVersion("0.1.9583.16545")]
 
 [assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
